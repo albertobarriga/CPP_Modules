59158c46479982de5cbd78cd84076aea3af41fb8
abarriga <abarriga4242@gmail.com>
2023-06-28 18:09:15 +0200

    doing bonus mod03

--- CPP_module03/ex03/DiamonTrap.cpp
+++ CPP_module03/ex03/DiamonTrap.cpp
@@ -1,41 +1,50 @@
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(): ClapTrap()
+DiamondTrap::DiamondTrap(): ClapTrap("defaultClassTrap")
 {
-	this->_hit_points = 100;
-	this->_energy_points = 100;
-	this->_attack_points = 30;
+	this->_name = "defaultClassTrap";
+	this->_hit_points = FragTrap::_hit_points;
+	this->_energy_points = ScavTrap::_energy_points;
+	this->_attack_points = FragTrap::_attack_points;
 	std::cout << "DiamondTrap Constructor called" << std::endl;
 }
 
-DiamondTrap::DiamondTrap(FragTrap const &copy): ClapTrap(copy)
+DiamondTrap::DiamondTrap(DiamondTrap const &copy): ClapTrap(copy), ScavTrap(copy), FragTrap(copy)
 {
+	*this = copy;
 	std::cout << "FragTrap call copy constructor" << std::endl;
 }
 
-FragTrap::FragTrap(std::string name): ClapTrap(name)
+DiamondTrap::DiamondTrap(std::string name): ClapTrap(name)
 {
-	this->_hit_points = 100;
-	this->_energy_points = 50;
-	this->_attack_points = 20;
+	this->_name = name;
+	this->_hit_points = FragTrap::_hit_points;
+	this->_energy_points = ScavTrap::_energy_points;
+	this->_attack_points = FragTrap::_attack_points;
 	std::cout << "FragTrap Constructor for the name " << this->_name << " called" << std::endl;
 }
 
-FragTrap::~FragTrap()
+DiamondTrap::~DiamondTrap()
 {
 	std::cout << "FragTrap destructor called" << std::endl;
 }
 
-FragTrap	&FragTrap::operator=(FragTrap const &copy)
+DiamondTrap	&DiamondTrap::operator=(DiamondTrap const &copy)
 {
-	std::cout << "FragTrap operator called" << std::endl;
+	std::cout << "DiamondTrap operator called" << std::endl;
+	this->_name = copy.name  + "_clap_name";
 	this->_hit_points = copy._hit_points;
 	this->_energy_points = copy._energy_points;
 	this->_attack_points = copy._attack_points;
 	return (*this);
 }
 
-void	FragTrap::highFivesGuys()
+void	DiamondTrap::attack(const std::string &target)
 {
-	std::cout << "High Fives Guys" << std::endl;
+	ScavTrap::attack(target);
+}
+
+void	DiamondTrap::whoAmI()
+{
+	std::cout << "Hello name" << std::endl;
 }
