9ac32bb096b0c2b5711055db31fc441e0c2b9545
abarriga <abarriga4242@gmail.com>
2023-07-14 16:19:20 +0200

    chanege main mod02 ex03

--- CPP_module02/ex03/main.cpp
+++ CPP_module02/ex03/main.cpp
@@ -5,10 +5,10 @@
 int main( void ) {
 
 	{
-	const Point a(-8, 7);
+	const Point a(0, 0);
 	const Point b(10, 0);
-	const Point c(0, 10);
-	const Point point(3, 5);
+	const Point c(5, 5);
+	const Point point(4, 3);
 
 	if (bsp(a, b, c, point) == true)
 		std::cout << "\033[32mTRUE\033[0m" << std::endl;
