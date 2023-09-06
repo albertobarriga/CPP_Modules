3df13bf5ea806ca8e95db2a47d483d425269072e
abarriga <abarriga4242@gmail.com>
2023-09-06 14:14:19 +0200

    mod08 finish

--- CPP_module08/ex02/main.cpp
+++ CPP_module08/ex02/main.cpp
@@ -1,4 +1,5 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
@@ -20,9 +21,33 @@ int main()
 	--it;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
 	std::stack<int> s(mstack);
+
+
+
+	std::cout << std::endl << "Begin of another container to check : " << std::endl << std::endl;
+	std::list<int> mlist;
+	mlist.push_front(5);
+	mlist.push_front(17);
+	std::cout << "top: " << mlist.front() << std::endl;
+	mlist.pop_front();
+	std::cout << "size: " << mlist.size() << std::endl;
+	mlist.push_front(3);
+	mlist.push_front(5);
+	mlist.push_front(737);
+	//[...]
+	mlist.push_front(0);
+	std::cout << std::endl << "print the up to down: " << std::endl << std::endl;
+
+
+	std::list<int>::iterator iter;
+	for (iter = --mlist.end(); iter != --mlist.begin(); iter--)
+	{
+		std::cout << *(iter) << std::endl;
+	}
+	// std::stack<int> s(mlist);
 	return 0;
 }
